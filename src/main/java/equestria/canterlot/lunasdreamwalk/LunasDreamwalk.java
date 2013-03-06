dad24367919201e6039b2d255c4cab4316dd2f67
Evenprime <wilfried.pasquazzo@gmail.com>
2013-03-06 22:13:18 +0100

    version 1.3 release

--- src/main/java/equestria/canterlot/lunasdreamwalk/LunasDreamwalk.java
+++ src/main/java/equestria/canterlot/lunasdreamwalk/LunasDreamwalk.java
@@ -43,7 +43,7 @@ public class LunasDreamwalk extends JFrame {
      */
     private static final long serialVersionUID = 509117784292343936L;
 
-    private static final String version = "1.2";
+    private static final String version = "1.3";
 
     private JButton load, retry, save;
     private JTextField imeiGluid, key, fileLocation;
